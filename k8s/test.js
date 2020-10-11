74a09b8c0231acee848305147d7b293366e7490b
PattamSetty, KrishnaSabaris [External] <KrishnaSabaris.PattamSetty2@externals.adidas.com>
2020-10-11 14:03:18 +0530

    updated jenkins for docker

---
+++ k8s/test.js
@@ -0,0 +1,35 @@
+class test{
+printed(msg){
+  console.log(`test message: ${msg}`)
+  }
+}
+const t = new test()
+t.printed("welcome to classes")
+console.log("how are you")
+console.log("i
+m good")
+console.log("how are you")
+console.log("i'm good")
+
+const printnums = arr =>{
+const printnums = arr =>{arr.forE
+
+const printnums = arr =>{
+arr.forEach(num => console.log(num))
+}
+printnums([1,2,3,4,5])
+
+let messages=[
+"A change of environment can be a good thing!",
+  "You will make it!",
+  "Just run with the code!"
+];
+
+console.log(messages)
+
+messages
+messages.forEach(msg =>{
+console.log(`${msg}`)
+
+}
+)
